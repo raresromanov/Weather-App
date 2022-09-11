0aa71a81276f25ccb327c82e18ea91cc2e3562b9
Rares Mihai Romanov <rares.romanov@netcentric.biz>
2022-09-11 13:15:06 +0300

    commit3

--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from datetime import datetime
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
 
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.shortcuts import render
-import requests
+
 from requests_html import HTMLSession
 
 
@@ -17,10 +10,13 @@ def get_html_content(city):
     url = f'https://www.google.com/search?q=weather+{city}'
     r = s.get(url, headers={
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'})
+    # print(r.html.xpath('img'))
+
     return r
 
 
 def home_view(request):
+    context = {}
     if 'city' in request.GET:
         city = request.GET.get('city')
         html_content = get_html_content(city)
@@ -30,13 +26,16 @@ def home_view(request):
         precipitatii = html_content.html.find('span#wob_pp', first=True).text
         umiditate = html_content.html.find('span#wob_hm', first=True).text
         locatie = html_content.html.find('div.wob_loc.q8U8x', first=True).text
+        image = html_content.html.find('div.UQt4rd img.wob_tci')[0]
+        image_url = image.attrs['src']
         context = {
             "temperatura": temp,
             "unitate": unit,
             "descriere": weather_type,
             "precipitatii": precipitatii,
             "umiditate": umiditate,
-            "locatie": locatie
+            "locatie": locatie,
+            "image_url": image_url
         }
-    return render(request, 'home.html', {"context":context})
+    return render(request, 'home.html', {'context': context})
 # Create your views here.
